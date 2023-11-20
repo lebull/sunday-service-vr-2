11726c95a226a0db509c762017824d58f151701c
Tyler Darsey <bulldogboy912000@yahoo.com>
2023-11-20 09:27:34 -0600

    update to typescript

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "SundayServiceTwo": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
